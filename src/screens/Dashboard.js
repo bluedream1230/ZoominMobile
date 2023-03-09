4f9b3d26d59ebdac353394ccaa1d98b0bab5d010
bluedream1230 <bluedream921230@gmail.com>
2023-03-09 13:53:29 -0500

    fix: change order of tab navigator

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -31,33 +31,20 @@ export default function Dashboard({ navigation }) {
           tabBarLabel: 'Home',
           tabBarIcon: () => (
             <Image
-              source={require('../assets/tabbaricons/home1.png')}
-              style={{ width: 24, height: 24 }}
+            source={require('../assets/tabbaricons/home1.png')}
+            style={{ width: 24, height: 24 }}
             />
-          ),
-        }}
-      /> */}
+            ),
+          }}
+        /> */}
       <Tab.Screen
-        name="Setting"
-        component={Setting}
+        name="Play"
+        component={Play}
         options={{
-          tabBarLabel: 'Setting',
+          tabBarLabel: 'Play',
           tabBarIcon: () => (
             <Image
-              source={require('../assets/tabbaricons/setting2.png')}
-              style={{ width: 24, height: 24 }}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Reward"
-        component={Reward}
-        options={{
-          tabBarLabel: 'Reward',
-          tabBarIcon: () => (
-            <Image
-              source={require('../assets/tabbaricons/reward1.png')}
+              source={require('../assets/tabbaricons/play2.png')}
               style={{ width: 24, height: 24 }}
             />
           ),
@@ -77,13 +64,26 @@ export default function Dashboard({ navigation }) {
         }}
       />
       <Tab.Screen
-        name="Play"
-        component={Play}
+        name="Reward"
+        component={Reward}
         options={{
-          tabBarLabel: 'Play',
+          tabBarLabel: 'Reward',
           tabBarIcon: () => (
             <Image
-              source={require('../assets/tabbaricons/play2.png')}
+              source={require('../assets/tabbaricons/reward1.png')}
+              style={{ width: 24, height: 24 }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Setting"
+        component={Setting}
+        options={{
+          tabBarLabel: 'Setting',
+          tabBarIcon: () => (
+            <Image
+              source={require('../assets/tabbaricons/setting2.png')}
               style={{ width: 24, height: 24 }}
             />
           ),
